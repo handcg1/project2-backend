78809467e2d8108ba1dec0f20ad3ee841f7be585
Callan <handcg@dukes.jmu.edu>
2020-11-17 18:54:36 +0000

    username endpoint

--- server/app.js
+++ server/app.js
@@ -11,6 +11,25 @@ let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 let connection = mysql.createConnection(credentials);
 connection.connect();
 
+function rowToObject(row) {
+  return {
+   username: row.username,
+   picture: row.picture,
+   caption: row.caption,
+  };
+}
+
+app.get('/post/:username', (request, response) => {
+  const query = 'SELECT username, picture, caption, id FROM post WHERE username = ? ORDER BY posted_at DESC';
+  const params = [request.params.username];
+  connection.query(query, params, (error, rows) => {
+   response.send({
+	ok: true,
+	post: rows.map(rowToObject),
+   });
+ });
+});
+
 const port = 3443;
 app.listen(port, () => {
     console.log(`We're live on port ${port}`);
