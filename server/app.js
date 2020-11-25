6456b1e05d2f2d9a44d9fbbed8c3594c8801684d
Callan <handcg@dukes.jmu.edu>
2020-11-25 13:02:30 +0000

    modified app.js to take images

--- server/app.js
+++ server/app.js
@@ -4,8 +4,12 @@ const mysql = require('mysql');
 const fs = require('fs');
 
 const app = express();
+const imageDirectory = '/home/handcg/image-uploads';
+//const fileupload = require('express-fileupload');
+
 app.use(cors());
 app.use(express.json());
+//app.use(file.upload());
 
 let credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
 let connection = mysql.createConnection(credentials);
@@ -22,13 +26,37 @@ function rowToObject(row) {
 app.get('/post/:username', (request, response) => {
   const query = 'SELECT username, picture, caption, id FROM post WHERE username = ? ORDER BY posted_at DESC';
   const params = [request.params.username];
+  const names = fs.readdirSync(imageDirectory);
+  //response.sendFile(`${imageDirectory}/${picture}`);
+  //response.sendFile();
   connection.query(query, params, (error, rows) => {
    response.send({
 	ok: true,
 	post: rows.map(rowToObject),
+        //image: names,
    });
- });
 });
+// response.send(names);
+});
+
+
+app.get('/images', (request, response) => {
+  const names = fs.readdirSync(imageDirectory);
+  response.send(names);
+});
+
+app.get('/image/:name', (request, response) => {
+  response.sendFile(`${imageDirectory}/${request.params.name}`);
+});
+
+
+
+/*
+app.get('/getimages', (request, response) => {
+   const names = fs.readdirSync(imageDirectory);
+   response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+   response.send(names);
+});*/
 
 app.post('/post', (request, response) => {
   const query = 'INSERT INTO post(username, picture, caption) VALUES (?, ?, ?)';
@@ -41,8 +69,6 @@ app.post('/post', (request, response) => {
                 });
         });
 });
-
-
 
 
 const port = 3443;
